56f9f0aabd01a5ea99e8811bc646b8f18e17f9d8
D2JS <d2js@TECHIN.INTERNAL>
2024-11-04 12:41:06 +0200

    automobilis

--- tinklapiu-kurimas/13-diena/Automobilis/automobilis.js
+++ tinklapiu-kurimas/13-diena/Automobilis/automobilis.js
@@ -1,2 +1,5 @@
 "use strict"
 const v = parseInt(prompt("Koks automobilio greitis?"));
+
+const s = 264 / (v * 1000) * 60 * 60;
+console.log("Automobilis tunelį pravažiuos per" + " " + s + " " +"s");
