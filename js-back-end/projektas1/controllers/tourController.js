5e7e01d4fbd2c1d7feee3ad13c65e2f3180972b5
D3JS <d3js@TECHIN.INTERNAL>
2025-01-13 15:11:24 +0400

    updated projektas1

--- js-back-end/projektas1/controllers/tourController.js
+++ js-back-end/projektas1/controllers/tourController.js
@@ -1,20 +1,33 @@
-const tours = require("../models/tourModel")
+// const tours = require("../models/tourModel")
+const {
+  getAllTours,
+  getTourById,
+  createTour,
+  patchTour,
+  eraseTour,
+} = require("../models/tourModel");
 
-exports.getAllTours = (req, res) => {
+//GET ALL TOURS
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await getAllTours();
     res.status(200).json({
-      //gali buti fail arba error
       status: "success",
-      count: tours.length,
-      date: req.requestedTime,
       data: tours,
     });
-  };
-  
-  exports.getTour = (req, res) => {
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+//GET TOUR BY ID
+exports.getTour = async (req, res) => {
+  try {
     const id = +req.params.id;
-  
-    const tour = tours.find((tour) => tour.id === id);
-  
+    const tour = await getTourById(id);
+
     if (!tour) {
       return res.status(404).json({
         status: "fail",
@@ -25,62 +38,68 @@ exports.getAllTours = (req, res) => {
       status: "success",
       data: tour,
     });
-  };
-  
-  exports.postTour = (req, res) => {
-    // console.log(req.body);
-  
-    const newID = tours[tours.length - 1].id + 1;
-    const newTour = {
-      id: newID,
-      ...req.body,
-    };
-    tours.push(newTour);
-  
-    fs.writeFile(dir, JSON.stringify(tours), () => {
-      if (err) {
-        return res.status(500).json({
-          status: "fail",
-          message: "Error writing file",
-        });
-      }
-      res.status(201).json({
-        status: "success",
-        data: newTour,
-      });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
     });
-  };
-  
-  exports.updateTour = (req, res) => {
-    const id = +req.params.id;
-  
-    if (id > tours.length) {
+  }
+};
+//CREATE TOUR
+exports.postTour = async (req, res) => {
+  try {
+    const tour = req.body;
+    const newTour = await createTour(tour);
+    res.status(200).json({
+      status: "success",
+      data: newTour,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+//EDIT TOUR
+exports.updateTour = async (req, res) => {
+  const id = +req.params.id;
+  try {
+    const tour = req.body;
+    const editTour = await patchTour(id, tour);
+    //issiunciamas atsakymas
+    res.status(200).json({
+      status: "success",
+      data: `Tour updated, Id: ${id}`, //newTour,
+      editedTour: editTour,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+//DELETE TOUR
+exports.deleteTour = async (req, res) => {
+  const id = +req.params.id;
+  try {
+    const deletedTour = await eraseTour(id);
+    if (deletedTour) {
+      res.status(200).json({
+        status: "success",
+        data: deletedTour,
+      });
+    } else {
       res.status(404).json({
         status: "fail",
         message: "Invalid ID",
       });
     }
-    //requesting body
-    const newTour = req.body;
-    //issiunciamas atsakymas
-    res.status(200).json({
-      status: "success",
-      data: `Tour updated, Id: ${id}`, //newTour,
-    });
-  };
-  
-  exports.deleteTour = (req, res) => {
-    const id = +req.params.id;
-  
-    const filteredTours = tours.filter((tour) => tour.id !== Number(id));
-    if (id < tours.length + 1) {
-      res.status(200).json({
-        status: "success",
-        data: filteredTours,
-      });
-    }
-    res.status(404).json({
+  } catch (error) {
+    res.status(500).json({
       status: "fail",
-      message: "Invalid ID",
+      message: error.message,
     });
-  };
+  }
+};
--- js-back-end/projektas1/models/tourModel.js
+++ js-back-end/projektas1/models/tourModel.js
@@ -1,8 +1,73 @@
-const fs = require("fs");
-const path = require("path");
+//IF WITHOUT DB =>
+// const fs = require("fs");
+// const path = require("path");
 
-const dir = path.join(__dirname, "../data/tours-simple.json");
+// const dir = path.join(__dirname, "../data/tours-simple.json");
 
-const tours = JSON.parse(fs.readFileSync(dir));
+// const tours = JSON.parse(fs.readFileSync(dir));
 
-module.exports = tours;
+// module.exports = tours;
+const { sql } = require("../dbConnection");
+
+//GET ALL TOURS
+exports.getAllTours = async () => {
+  const tourList = await sql`
+    SELECT tours.*
+    FROM tours
+    `;
+
+  return tourList;
+};
+
+//GET TOUR BY ID
+exports.getTourById = async (id) => {
+  const tours = await sql`
+SELECT tours.*
+FROM tours
+WHERE tours.id = ${id}
+`;
+
+  return tours[0]; //tours is array
+};
+
+//CREATE TOUR
+exports.createTour = async (tour) => {
+  const columns = [
+    "name",
+    "description",
+    "category",
+    "price",
+    "duration",
+    "difficulty",
+  ];
+
+  const postTour = await sql`
+    insert into tours ${sql(tour, columns)}
+    RETURNING *
+    `;
+
+  return postTour[0];
+};
+
+//EDIT TOUR
+exports.patchTour = async (id, tour) => {
+  const columns = Object.keys(tour);
+  const editTour = await sql`
+    update tours set ${sql(tour, columns)}
+    where tours.id = ${id}
+    RETURNING *
+    `;
+
+  return editTour[0];
+};
+
+//DELETE TOUR
+exports.eraseTour = async (id) => {
+  const removeTour = await sql`
+    delete from tours 
+    where tours.id = ${id}
+    RETURNING *
+    `;
+
+  return removeTour[0];
+};
